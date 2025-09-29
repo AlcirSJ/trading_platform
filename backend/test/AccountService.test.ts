baa968ef9db3675765e5799b4986c4124388134e
esoft02 <ex188928@g.unicamp.br>
2025-09-29 20:24:49 -0300

    tests

--- backend/test/AccountService.test.ts
+++ backend/test/AccountService.test.ts
@@ -1,12 +1,13 @@
-import { AccountDAOMemory } from "../src/AccountDAO";
+import sinon from "sinon";
+import { AccountDAODatabase, AccountDAOMemory } from "../src/AccountDAO";
 import AccountService from "../src/AccountService";
 
 
 let accountService: AccountService;
 
 beforeEach(() => {
-    // const accountDAO = new AccountDAODatabase();
-    const accountDAO = new AccountDAOMemory();
+     const accountDAO = new AccountDAODatabase();
+    //const accountDAO = new AccountDAOMemory();
     accountService = new AccountService(accountDAO);
 });
 
@@ -64,4 +65,87 @@ test("Não deve criar uma conta se a senha tiver menos de 8 caracteres", async (
         password: "asdQWE"
     }
     await expect(() => accountService.signup(input)).rejects.toThrow(new Error("Invalid password"));
+});
+
+test("Deve criar uma conta com stub", async () => {
+    const saveStub = sinon.stub(AccountDAODatabase.prototype, "save").resolves();
+    const input = {
+        name: "John Doe",
+        email: "john.doe@gmail.com",
+        document: "97456321558",
+        password: "asdQWE123"
+    }
+    const getByIdStub = sinon.stub(AccountDAODatabase.prototype, "getById").resolves(input);
+    const outputSignup = await accountService.signup(input);
+    const outputGetAccount = await accountService.getAccount(outputSignup.accountId);
+    expect(outputSignup.accountId).toBeDefined();
+    expect(outputGetAccount.name).toBe(input.name);
+    expect(outputGetAccount.email).toBe(input.email);
+    expect(outputGetAccount.document).toBe(input.document);
+    expect(outputGetAccount.password).toBe(input.password);
+    saveStub.restore();
+    getByIdStub.restore();
+});
+
+
+test("Deve criar uma conta com spy", async () => {
+    const saveSpy = sinon.spy(AccountDAODatabase.prototype, "save");
+    const getByIdSpy = sinon.spy(AccountDAODatabase.prototype, "getById");
+    const input = {
+        name: "John Doe",
+        email: "john.doe@gmail.com",
+        document: "97456321558",
+        password: "asdQWE123"
+    }
+    const outputSignup = await accountService.signup(input);
+    const outputGetAccount = await accountService.getAccount(outputSignup.accountId);
+    expect(outputSignup.accountId).toBeDefined();
+    expect(outputGetAccount.name).toBe(input.name);
+    expect(outputGetAccount.email).toBe(input.email);
+    expect(outputGetAccount.document).toBe(input.document);
+    expect(outputGetAccount.password).toBe(input.password);
+    expect(saveSpy.calledOnce).toBe(true);
+    expect(getByIdSpy.calledWith(outputSignup.accountId)).toBe(true);
+    expect(getByIdSpy.calledOnce).toBe(true);
+    saveSpy.restore();
+    getByIdSpy.restore();
+});
+
+test("Deve criar uma conta com mock", async () => {
+    const accountDAOMock = sinon.mock(AccountDAODatabase.prototype);
+    accountDAOMock.expects("save").once().resolves();
+    const input = {
+        name: "John Doe",
+        email: "john.doe@gmail.com",
+        document: "97456321558",
+        password: "asdQWE123"
+    }
+    accountDAOMock.expects("getById").once().resolves(input);
+    const outputSignup = await accountService.signup(input);
+    const outputGetAccount = await accountService.getAccount(outputSignup.accountId);
+    expect(outputSignup.accountId).toBeDefined();
+    expect(outputGetAccount.name).toBe(input.name);
+    expect(outputGetAccount.email).toBe(input.email);
+    expect(outputGetAccount.document).toBe(input.document);
+    expect(outputGetAccount.password).toBe(input.password);
+    accountDAOMock.verify();
+    accountDAOMock.restore();
+});
+
+test("Deve criar uma conta com fake", async () => {
+    const accountDAO = new AccountDAOMemory();   
+    accountService = new AccountService(accountDAO);
+    const input = {
+        name: "John Doe",
+        email: "john.doe@gmail.com",
+        document: "97456321558",
+        password: "asdQWE123"
+    }
+    const outputSignup = await accountService.signup(input);
+    const outputGetAccount = await accountService.getAccount(outputSignup.accountId);
+    expect(outputSignup.accountId).toBeDefined();
+    expect(outputGetAccount.name).toBe(input.name);
+    expect(outputGetAccount.email).toBe(input.email);
+    expect(outputGetAccount.document).toBe(input.document);
+    expect(outputGetAccount.password).toBe(input.password);
 });
